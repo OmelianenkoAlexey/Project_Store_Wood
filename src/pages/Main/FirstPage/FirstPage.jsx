bc8b253cdd405f7afbfc602392651a7fece71b3e
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-07-22 09:47:02 +0200

    Alex firstPage

---
+++ src/pages/Main/FirstPage/FirstPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FirstPage.css';
+
+export default function FirstPage() {
+
+  return (
+    <div className='firstPage'>
+
+      <div className='firstPage-box container'>
+        <h2 className='firstPage-box__title'>StoreWood</h2>
+        <div className='firstPage-box__description'>якісні дитячі ліжка та меблі</div>
+      </div>
+      <div className='firstPage-background'></div>
+    </div>
+  );
+}
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import ImageCarousel from './ImageCarousel/ImageCarousel';
 import './Main.css';
+import FirstPage from './FirstPage/FirstPage';
 
 export default function Main() {
   return (
     <main>
       {/* <ImageCarousel /> */}
+      <FirstPage />
     </main>
   );
 }
