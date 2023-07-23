f148456f678ab2301c3c499b4906e9a72566f9a4
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-07-23 14:42:46 +0200

    Alex 2

--- src/pages/Main/FirstPage/FirstPage.jsx
+++ src/pages/Main/FirstPage/FirstPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './FirstPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faFacebookF, faInstagram, faSquareFacebook, faViber } from '@fortawesome/free-brands-svg-icons';
 
 export default function FirstPage() {
 
@@ -9,6 +11,22 @@ export default function FirstPage() {
       <div className='firstPage-box container'>
         <h2 className='firstPage-box__title'>StoreWood</h2>
         <div className='firstPage-box__description'>якісні дитячі ліжка та меблі</div>
+        <div className='firstPage-box__btn'>Замовити дизайн</div>
+        <div className='firstPage-box__social'>
+          <a className='firstPage-box__social-item' href="https://www.instagram.com/legenda_parfum/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} className='firstPage-box__social-icon' />
+          </a>
+          <a className='firstPage-box__social-item' href="https://www.instagram.com/legenda_parfum/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faFacebookF} className='firstPage-box__social-icon' />
+            {/* <FontAwesomeIcon icon={faFacebook} className='firstPage-box__social-icon' /> */}
+          </a>
+          <a className='firstPage-box__social-item' href="https://www.instagram.com/legenda_parfum/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faViber} className='firstPage-box__social-icon' />
+          </a>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
       </div>
       <div className='firstPage-background'></div>
     </div>
