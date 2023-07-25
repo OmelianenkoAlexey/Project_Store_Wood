bd94af0918df1b91a19ccd05d47d849c4dfc9806
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-07-25 08:55:03 +0200

    Alex instagram 2

--- src/pages/Main/Instagram/Instagram.jsx
+++ src/pages/Main/Instagram/Instagram.jsx
@@ -9,9 +9,28 @@ export default function Instagram() {
         <img className='instagram-image' src="./img/catalogImage3.png" alt="Store Wood" />
       </div>
       <div className='instagram-container container'>
-      </div>
 
-      <h2>Instagram</h2>
+        <h2 className='instagram__title'>Наш інстаграм</h2>
+        <a className='instagram__link' href='https://www.instagram.com/legenda_parfum/' target="_blank" rel="noreferrer">@storewood</a>
+
+        <div className='instagram-box'>
+          <div className='instagram-box__item'>
+            <img className='instagram-box__item-image' src="./img/instagram1.png" alt="Store Wood" />
+          </div>
+
+          <div className='instagram-box__item'>
+            <img className='instagram-box__item-image' src="./img/instagram2.png" alt="Store Wood" />
+          </div>
+
+          <div className='instagram-box__item'>
+            <img className='instagram-box__item-image' src="./img/instagram3.png" alt="Store Wood" />
+          </div>
+
+          <div className='instagram-box__item'>
+            <img className='instagram-box__item-image' src="./img/instagram1.png" alt="Store Wood" />
+          </div>
+        </div>
+      </div>
 
       <div className='firstPage-background'></div>
     </div>
