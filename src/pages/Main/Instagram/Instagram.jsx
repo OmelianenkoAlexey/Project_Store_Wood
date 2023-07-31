88c4155a63c70c75d8b40bcac48793d7c67ba7df
glinkinzhenya <glinkinzhenya@gmail.com>
2023-07-31 12:55:39 +0300

    bjhbjbh

--- src/pages/Main/Instagram/Instagram.jsx
+++ src/pages/Main/Instagram/Instagram.jsx
@@ -1,14 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useMediaQuery } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Context } from '../../../Contex';
 import './Instagram.css';
 
 export default function Instagram() {
-
+  const { mainData } = useContext(Context);
   const [positionOne, setPositionOne] = useState(false);
   const [click, setClick] = useState(false);
   const [image, setImage] = useState(false);
+  const [data, setData] = useState([]);
+  const [img, setImg] = useState([]);
+
+
+  const loadImage = (src) => new Promise((resolve, reject) => {
+    const image = new Image();
+    image.onload = () => resolve(image); // Вместо src возвращаем сам объект изображения
+    image.onerror = (error) => reject(error);
+    image.src = src;
+  });
 
   useEffect(() => {
     if (positionOne) {
@@ -21,6 +32,33 @@ export default function Instagram() {
       });
     }
   }, [positionOne]);
+
+  useEffect(() => {
+
+    if (mainData) {
+      setData(mainData[0].carousel);
+    }
+  }, [mainData]);
+
+
+  useEffect(() => {
+    const loadImages = async () => {
+      try {
+        const loadedImages = await Promise.all(data.map((item) => loadImage(item)));
+        // Здесь loadedImages будет массивом объектов изображений, которые успешно загрузились.
+        // Вы можете выполнить дополнительные действия с загруженными изображениями здесь.
+        console.log(loadedImages);
+        setImg(loadedImages);
+      } catch (error) {
+        // Обработка ошибок при загрузке изображений
+        console.error(error);
+      }
+    };
+
+    loadImages();
+  }, [data]);
+
+
 
 
   useEffect(() => {
@@ -51,24 +89,20 @@ export default function Instagram() {
   }, []);
 
 
-  // новый код
-
-  const mainData2 = ['./img/1.png', './img/2.png', './img/3.png', './img/4.png', './img/5.png', './img/6.png', './img/7.png'];
-
   const [index, setIndex] = useState(0);
 
-
   const handleClickLeft = () => {
-      const newIndex = index === 0 ? mainData2.length - 1 : index - 1;
-      setIndex(newIndex);
-  };
-
-  const handleClickRight = () => {
-    const newIndex = index === mainData2.length - 1 ? 0 : index + 1;
+    const newIndex = index === 0 ? img.length - 1 : index - 1;
     setIndex(newIndex);
   };
 
-  const newNews = [...mainData2.slice(index, mainData2.length), ...mainData2.slice(0, index)];
+  const handleClickRight = () => {
+    const newIndex = index === img.length - 1 ? 0 : index + 1;
+    setIndex(newIndex);
+  };
+
+  const newNews = [...img.slice(index, img.length), ...img.slice(0, index)];
+  console.log(newNews);
 
   const handleClickOpen = (item) => {
     setClick(true);
@@ -122,7 +156,7 @@ export default function Instagram() {
             {Screen768
               ? newNews.slice(0, 4).map((item, index) => (
                 <div onClick={() => handleClickOpen(item)} key={index} className='instagram-box__item'>
-                  <img className='instagram-box__item-image' src={item} alt="" />
+                  <img className='instagram-box__item-image' src={item.currentSrc} alt="" />
                 </div>
               ))
               : newNews.slice(0, 2).map((item, index) => (
