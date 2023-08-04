28228af63155f3a411b2e9c959f56bbfc5836468
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-08-04 10:03:44 +0200

    Alex productItem 5

--- src/pages/Main/Catalog/ProductItem/ProductItem.jsx
+++ src/pages/Main/Catalog/ProductItem/ProductItem.jsx
@@ -16,14 +16,14 @@ export default function ProductItem() {
           </div>
 
           <div className='product-window__info'>
-            <h2 className='product-window__info-title'>Кровать с пружинным матрасом SOFTY</h2>
+            <h2 className='product-window__info-title'>Кровать с пружинным матрасом SOFTY SOFTY SOFTY SOFTY SOFTY SOFTY SOFTY SOFTY SOFTY SOFTY SOFTY</h2>
             <div className='product-window__info-price'>€ 999,00</div>
             <div className='product-window__info-color'>Цвет</div>
             <div className='product-window__info-color-box color-box'>
-              <div className='color-box__item'></div>
-              <div className='color-box__item'></div>
-              <div className='color-box__item'></div>
-              <div className='color-box__item'></div>
+              <div className='color-box__item item1'></div>
+              <div className='color-box__item item2'></div>
+              <div className='color-box__item item3'></div>
+              <div className='color-box__item item4'></div>
             </div>
 
             <div className="accordion" id="accordionExample">
@@ -35,7 +35,8 @@ export default function ProductItem() {
                 </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                   <div className="accordion-body">
-                    <div className='product-window__info-description'>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.</div>
+                    <div className='product-window__info-description'>Мы разработали модель SOFTY для любителей эксклюзивных и удобных кроватей, как в королевских покоях. Эта мягкая и теплая континентальная кровать идеально подходит для спален в стиле барокко и гламур, а также для комнаты принцессы подростка.
+                      Континентальная кровать SOFTY является синонимом комфорта и роскоши. Он отличается тонкой строчкой и необычной тафтинговой строчкой. SOFTY имеет просторную лежачую поверхность, так что спать в ней становится сплошным удовольствием. Элегантное изголовье кровати SOFTY придает очарование любой спальне и обеспечивает комфортную поддержку спины. Вы можете оформить свою кровать SOFTY в соответствии со своими пожеланиями и самостоятельно выбрать размер и цвет покрытия.</div>
                   </div>
                 </div>
               </div>
@@ -47,7 +48,16 @@ export default function ProductItem() {
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                   <div className="accordion-body">
-                    <div className='product-window__info-description'>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.y</div>
+                    <div className='product-window__info-description'>Полная длина	207 см
+                      Общая высота	122 см
+                      лежачая поверхность 	200см
+                      общая ширина	142/162/182 см
+                      ширина матраса	140/160/180
+                      лежачая высота	65 см
+                      место для хранения
+                      С местом для хранения
+                      ножки кровати
+                      хромированные ножки</div>
                   </div>
                 </div>
               </div>
